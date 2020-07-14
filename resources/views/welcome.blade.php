2802f4b45a7048b7e5d4e72e50514c07f8ac8007
zhaoning <56795657@qq.com>
2020-07-14 16:51:19 +0800

    one

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    水滴彡
                 </div>
 
                 <div class="links">
